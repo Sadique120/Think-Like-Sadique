ca0811652c31b3a0b09bfb443681bf818171a335
sadique120 <sadiqueiqrar88@gmail.com>
2026-04-21 07:08:42 +0530

    CORS fixes

--- backend/server.js
+++ backend/server.js
@@ -10,19 +10,7 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
-
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  }),
-);
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
